46549907473e792f95444b069df00c1e1fc0ab97
tengrui <tengrui13@gmail.com>
2020-05-05 00:28:48 -0400

    Updates

---
+++ precache-manifest.3cae03b9f649135ad4201981c30db65b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2a4f16ce9281cd9aa8355988c144e0bf",
+    "url": "/recipe/index.html"
+  },
+  {
+    "revision": "9195fe0571f6fabb7839",
+    "url": "/recipe/static/css/main.88d1d1dd.chunk.css"
+  },
+  {
+    "revision": "afc206ece68bc0564db7",
+    "url": "/recipe/static/js/2.ba81a6e7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/recipe/static/js/2.ba81a6e7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9195fe0571f6fabb7839",
+    "url": "/recipe/static/js/main.010467f1.chunk.js"
+  },
+  {
+    "revision": "e1c49278ddd3267ffd5d",
+    "url": "/recipe/static/js/runtime-main.3d555b5a.js"
+  }
+]);
